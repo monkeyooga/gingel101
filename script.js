80047ca2a35e98f2b5cf3be3ef7dda02be61ab74
monkeyooga <giancarloramirez@yahoo.com>
2026-07-16 12:28:12 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function checkPassword() {
-    let password = document.getElementById("password").value;
-
-    if (password === "250723") {
-        window.location.href = "index.html";
-    } else {
-        alert("Wrong password");
-    }
-}
